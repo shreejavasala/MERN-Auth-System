9e81eda241b83e8fd358371ca2eae811068525b7
shreejavasala <shreevasala29@gmail.com>
2025-08-29 13:46:09 +0530

    Create User Schema model

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 import { connectDB } from './db/connectDB.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
-
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!")
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
   connectDB();
