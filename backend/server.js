de0da1e2df16a44e2cb2687c6a67deffd1929e38
shreejavasala <shreevasala29@gmail.com>
2025-08-29 13:18:50 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!")
+});
+
+app.listen(PORT, () => console.log(`Server is running of PORT: ${PORT}`));
