76f8765200ade05585e15fd2a060407ca36168e7
shreejavasala <shreevasala29@gmail.com>
2025-08-30 16:47:42 +0530

    Create EmailVerificationPage

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 import { connectDB } from './db/connectDB.js';
 import authRouter from './routes/auth.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json())
 app.use(cookieParser());
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
 
 function App() {
   
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
       <Routes>
+        <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage /> } />
       </Routes>
     </div>
   )
