9b00aa3fbfc7c5899b9227315d4c40f53c02280c
shreejavasala <shreevasala29@gmail.com>
2025-08-30 22:32:37 +0530

    Delete vercel.json

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "/frontend/dist/index.html"));
   });
 } 
 
