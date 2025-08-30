ea0c506ace35339a8e37036caf58dfe4bb1d9bd1
shreejavasala <shreevasala29@gmail.com>
2025-08-30 18:23:12 +0530

    Create DashboardPage and add logout functionality

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -37,6 +37,17 @@ export const useAuthStore = create((set) => ({
     }
   },
 
+  logout: async () => {
+    set({ isLoading: true, error: null })
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({ user: null, isAuthenticated: false, isLoading: false });
+    } catch (error) {
+      set({ error: "Error logging out", isLoading: false });
+      throw error;
+    }
+  },
+
   verifyEmail: async (code) => {
     set({ isLoading: true, error: null });
     try {
